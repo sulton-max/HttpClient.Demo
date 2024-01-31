69363db80279bd2ab92fe89d31439965f68bfa8f
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-01-31 18:29:18 +0500

    Add host configurations

---
+++ src/N1.ClientUsage/Configurations/HostConfiguration.cs
@@ -0,0 +1,28 @@
+﻿namespace N1.ClientUsage.Configurations;
+
+public static partial class HostConfiguration
+{
+    /// <summary>
+    /// Configures application builder
+    /// </summary>
+    /// <param name="builder">Application builder</param>
+    /// <returns>Application builder</returns>
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder.AddDevTools().AddExposers();
+
+        return new ValueTask<WebApplicationBuilder>(builder);
+    }
+
+    /// <summary>
+    /// Configures application
+    /// </summary>
+    /// <param name="app">Application host</param>
+    /// <returns>Application host</returns>
+    public static ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        app.UseDevTools().UseCors().UseStaticFiles();
+
+        return new ValueTask<WebApplication>(app);
+    }
+}
